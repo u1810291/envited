67c30af6f54a0295cff99d66c029d005b2fdca7d
VRIZAOT <otabekbutcher@gmail.com>
2022-09-24 17:55:07 +0500

    init: initiating project

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Button = ({ title }: { title: string }) => {
+  return (
+    <div>{title}</div>
+  )
+}
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import { ContentButton } from '../ContentButton'
+
+export function Card() {
+  return (
+    <div className="card">
+      <h1>Card</h1>
+      <ContentButton />
+    </div>
+  )
+}
